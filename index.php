2d1fd56d541bd0bed8c227a40e4b69424840b670
Black.Hacker <farisksa79@gmail.com>
2019-08-02 00:19:17 +0300

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -1,22 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-   <title>EnterpriseCo - الرئيسيه</title>
-   <meta charset="UTF-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-   <link rel="shortcut icon" href="http://icons.iconarchive.com/icons/chanut/role-playing/128/Monster-icon.png">
-   <link rel="stylesheet" href="src/css/bootstrap.min.css">
-   <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-   <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
-   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-   <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
- <!-- RTL Support -->
-   <link rel="stylesheet" href="//cdn.rawgit.com/morteza/bootstrap-rtl/v3.3.4/dist/css/bootstrap-rtl.min.css">
-   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.css">
-   <link rel="stylesheet" type="text/css" href="src/css/custom.css">
-   <script type="text/javascript" src="src/js/scrool-smooth.js"></script>
-   <script type="text/javascript" src="src/js/imgload.js"></script>
-   <script type="text/javascript" src="src/js/cookies.js"></script>
+   <title>EnterpriseCo - Home</title>
+   <?php include "tags.php" ?>
 </head>
 
 <body data-easing="easeInQuad">
