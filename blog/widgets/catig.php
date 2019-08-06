c5ed33ecd9151fe254918b5c6cfa1855742053f8
BlackHacker511 <farisksa79@gmail.com>
2019-08-06 04:27:10 +0300

    new Fixes

--- blog/widgets/catig.php
+++ blog/widgets/catig.php
@@ -1,30 +1,22 @@
+ <?php  
+include '../functions/cat.php';
+$cat = new Categories;
+$getCat = $cat->getCats();
+ ?>
  <div class="card my-4">
           <h5 class="card-header">Categories</h5>
           <div class="card-body">
             <div class="row">
               <div class="col">
                 <ul class="list-unstyled mb-0">
-                  <li>
-                    <a class="text-decoration-none" href="#">Web Design</a>
-                  </li>
-                  <li>
-                    <a class="text-decoration-none"  href="#">HTML</a>
-                  </li>
-                  <li>
-                    <a class="text-decoration-none" href="#">Freebies</a>
-                  </li>
-                  <li>
-                    <a class="text-decoration-none" href="#">JavaScript</a>
-                  </li>
-                  <li>
-                    <a class="text-decoration-none" href="#">CSS</a>
-                  </li>
-                  <li>
-                    <a class="text-decoration-none" href="#">Tutorials</a>
-                  </li>
-                  <li>
-                    <a  class="text-decoration-none" href="#">PHP</a>
-                  </li>
+
+                  <?php
+                  foreach ($getCat as $Categorie) {
+                    echo '<li>
+                            <a class="text-decoration-none" href="category.php?id='.$Categorie->slug.'">'.$Categorie->name.'</a>
+                          </li>';
+                  }
+                  ?>
                 </ul>
               </div>
             </div>
--- functions/config.inc.php
+++ functions/config.inc.php
@@ -3,16 +3,16 @@ class Database{
 	private $username = "root";
 	private $password = "farisksa511";
 	private $host = "localhost";
-	private $db_name = "panel";
+	private $db_name = "EnterpriseCo";
 	private $charset = "utf8";
 
 	public function Connect(){
 
     try{
-	$dsn = "mysql:host=".$this->host.";dbname=".$this->db_name.";charset=".$this->charset;
-	$pdo = new PDO($dsn, $this->username, $this->password,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));	
-	$pdo -> setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
-	$pdo->exec("SET CHARACTER utf8");
+		$dsn = "mysql:host=".$this->host.";dbname=".$this->db_name.";charset=".$this->charset;
+		$pdo = new PDO($dsn, $this->username, $this->password,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));	
+		$pdo -> setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+		$pdo->exec("SET CHARACTER utf8");
 	}
 	  catch(PDOException $err){
       	die('Unable to connect: '.$err->getMessage());
